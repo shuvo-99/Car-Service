7e2db6917185684e9ab76ff263c7e2f913fcb95d
shuvo-99 <anontoshuvo99@gmail.com>
2023-12-26 18:26:27 +0600

    update

--- frontend/src/Pages/Shared/Navbar/Navbar.jsx
+++ frontend/src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import logo from "";
+import logo from "../../../assets/logo.svg";
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <img src={logo} alt="" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
           <ul
             tabIndex={0}
@@ -31,7 +44,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <img src={logo} alt="" />
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
